cfbac7a3249ea02ffe826fe01295c9557b4c8822
gangus22 <80174357+gangus22@users.noreply.github.com>
2023-04-26 20:45:31 +0200

    Firebase poc (#2)
    * added firebase post query POC
    
    * added cool new icons + login fix

--- app/src/main/java/com/example/birbapp/posts/PostModel.java
+++ app/src/main/java/com/example/birbapp/posts/PostModel.java
@@ -1,14 +1,18 @@
 package com.example.birbapp.posts;
 
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+
 public class PostModel {
-    //TODO: add other shit
-    public String authorId;
+    public DocumentReference author;
+
+    public Timestamp date;
     public String content;
     public int likes;
     public int reposts;
 
-    public PostModel(String authorId, String content, int likes, int reposts) {
-        this.authorId = authorId;
+    public PostModel(DocumentReference author, Timestamp date, String content, int likes, int reposts) {
+        this.author = author;
         this.content = content;
         this.likes = likes;
         this.reposts = reposts;
