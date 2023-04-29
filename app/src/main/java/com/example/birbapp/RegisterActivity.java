4bcb44ebb0d5e5aa1df72898a57ad9edbd861c06
gangus22 <80174357+gangus22@users.noreply.github.com>
2023-04-29 20:58:23 +0200

    added likes + reposts (#3)
    * added likes + reposts
    
    * removed useless imports

--- app/src/main/java/com/example/birbapp/RegisterActivity.java
+++ app/src/main/java/com/example/birbapp/RegisterActivity.java
@@ -1,21 +1,16 @@
 package com.example.birbapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.birbapp.user.UserModel;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Objects;
